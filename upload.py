daed8f935c91f6913164fedd1334cae177d04c97
Trevor Stanhope <trevor@T510>
2013-10-05 17:56:48 -0400

    org

--- upload.py
+++ upload.py
@@ -3,22 +3,23 @@ import json
 import time
 import couchdb
 import csv
+import sys
 
 class referid:
 
   ## connect to db
   def __init__(self):
-    database = raw_input("Enter database name: ")
+    DATABASE = sys.argv[1]
     try:
       server = couchdb.Server()
-      self.db = server.create(database) # if database doesn't exist
+      self.db = server.create(DATABASE) # if database doesn't exist
     except Exception:
-      self.db = server[database] # assuming database exists
+      self.db = server[DATABASE] # assuming database exists
   
   ## upload from csv
   def upload(self):
-    csvfile = raw_input('Enter CSV filename: ')
-    with open(csvfile + '.csv', 'rb') as csvfile:
+    CSVFILE = sys.argv[2]
+    with open(CSVFILE, 'rb') as csvfile:
       csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
       headers = csvreader.next() # get first row of headers
       for values in csvreader:
